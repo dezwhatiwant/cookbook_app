0a646bda1c1c8a7357631d8e4ec57e74810e6513
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-10 20:43:33 -0500

    added create, show, update, and destroy actions

--- runner.rb
+++ runner.rb
@@ -5,5 +5,44 @@ require 'unirest'
 # puts JSON.pretty_generate(response.body)
 
 # SHOW ACTION
-response = Unirest.get("http://localhost:3000/api/recipes/1")
-puts JSON.pretty_generate(response.body)
+# response = Unirest.get("http://localhost:3000/api/recipes/1")
+# puts JSON.pretty_generate(response.body)
+
+# CREATE ACTION
+# response = Unirest.post(
+#                         "http://localhost:3000/api/recipes", 
+#                         parameters: {title: "baked squirell",
+#                                      chef: "Bobbi Jo",
+#                                      ingredients: "roadkill",
+#                                      directions: "find it kill it and eat it",
+#                                      prep_time: 120
+#                                     }
+#                         )
+# puts JSON.pretty_generate(response.body)
+
+# UPDATE ACTION
+# recipe_id = 6
+# runner_params = {
+#                  title: "Mud Cake"
+#                 }
+
+# response = Unirest.patch(
+#                          "http://localhost:3000/api/recipes/#{recipe_id}",
+#                          parameters: runner_params
+
+#                         )
+# recipe_hash = response.body
+# puts JSON.pretty_generate(recipe_hash)
+
+# DESTROY ACTION
+recipe_id = 5
+response = Unirest.delete("http://localhost:3000/api/recipes/#{recipe_id}")
+data = response.body
+puts JSON.pretty_generate(data)
+
+
+
+
+
+
+
