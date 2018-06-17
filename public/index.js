05ca7a1bb630732b83d5a32513c465c23149ce9b
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-06-17 09:45:18 -0500

    added index html and js in backend

---
+++ public/index.js
@@ -0,0 +1,9 @@
+var recipeTemplate = document.querySelector('#recipe-card');
+var recipeRowElement = document.querySelector('.row');
+
+recipeRowElement.appendChild(recipeTemplate.content.cloneNode(true));
+recipeRowElement.appendChild(recipeTemplate.content.cloneNode(true));
+recipeRowElement.appendChild(recipeTemplate.content.cloneNode(true));
+recipeRowElement.appendChild(recipeTemplate.content.cloneNode(true));
+recipeRowElement.appendChild(recipeTemplate.content.cloneNode(true));
+recipeRowElement.appendChild(recipeTemplate.content.cloneNode(true));
