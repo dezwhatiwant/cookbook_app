bf0f399dc74bbea22a25f49eadd4371f8d5cfb8e
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-06-17 11:09:29 -0500

    added axios call

--- public/index.js
+++ public/index.js
@@ -1,9 +1,18 @@
 var recipeTemplate = document.querySelector('#recipe-card');
 var recipeRowElement = document.querySelector('.row');
 
-recipeRowElement.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRowElement.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRowElement.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRowElement.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRowElement.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRowElement.appendChild(recipeTemplate.content.cloneNode(true));
+axios.get("http://localhost:3000/api/recipes").then(function(response) {
+  var recipes = response.data;
+
+  recipes.forEach(function(recipe) {
+    var recipeClone = recipeTemplate.content.cloneNode(true);
+
+    recipeClone.querySelector(".card-title").innerText = recipe.title;
+    recipeClone.querySelector(".chef").innerText = recipe.chef;
+    recipeClone.querySelector(".prep-time").innerText = recipe.prep_time;
+    recipeClone.querySelector(".card-img-top").src = recipe.image_url;
+
+
+    recipeRowElement.appendChild(recipeClone);
+  });
+});
